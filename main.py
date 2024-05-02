0ec3d3caba354cdcfe766fa386fc9f8285799913
MarianaV1 <51389348+marianav1@users.noreply.github.com>
2024-05-01 21:51:53 -0600

    CORS add

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from db.db import client
 from controller.libroCRUD import router as libros_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "https://vega-usuarios-api.azurewebsites.net"
+    "http://vega-usuarios-api.azurewebsites.net"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Puedes especificar métodos específicos ['GET', 'POST', etc.]
+    allow_headers=["*"],
+)
+
 app.include_router(libros_router, tags=["libros"], prefix="/libros")
 # MongoDB connection URL
 @app.on_event("shutdown")
